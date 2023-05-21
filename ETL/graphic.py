17b807239c56ad21faa991f9323fff395074beed
Juan Marquesán <juanmarquesan@gmail.com>
2023-05-21 13:24:48 +0200

    Change structure

--- ETL/graphic.py
+++ ETL/graphic.py
@@ -5,48 +5,36 @@ from scipy.signal import argrelextrema
 from ETL.get_historic_prices import DataLoader
 from datetime import datetime
 
+
 class DataPlotter:
     def __init__(self, data_loader):
         self.data_loader = data_loader
 
     def plot_data(self):
-        # Load the data
         df = self.data_loader.load_data()
-
         if df is None:
-            print(f"Failed to load data for {self.data_loader.ticker}")
-            return
+            raise Exception(f"Failed to load data for {self.data_loader.ticker}")
 
-        # Set the size of the window for finding relative extrema
-        order = 50  # adjust this value based on your needs
-
-        # Find local maxima and minima
+        order = 50
         local_max = argrelextrema(df['Close'].values, np.greater_equal, order=order)
         local_min = argrelextrema(df['Close'].values, np.less_equal, order=order)
 
-        # Plot the data
         plt.figure(figsize=(10, 6))
         sns.lineplot(data=df, x=df.index, y='Close')
 
-        # Plot local maxima
         for index in local_max:
             plt.scatter(df.index[index], df['Close'].iloc[index], color='g')
 
-        # Plot local minima
         for index in local_min:
             plt.scatter(df.index[index], df['Close'].iloc[index], color='r')
 
         plt.title(f"Closing price of {self.data_loader.ticker}")
         plt.show()
 
+
 if __name__ == "__main__":
-    # Create an instance of DataLoader
     start_date = datetime(2020, 1, 1)
     end_date = datetime(2023, 5, 20)
     data_loader = DataLoader('BTC-USD', start_date, end_date)
-
-    # Create an instance of DataPlotter
     data_plotter = DataPlotter(data_loader)
-
-    # Plot the data
     data_plotter.plot_data()
--- main.py
+++ main.py
@@ -1,24 +1,20 @@
-import datetime
-from ETL.get_historic_prices import DataLoader
-from ETL.graphic import DataPlotter  # Asegúrate de que el path a graphic.py sea correcto
+from datetime import datetime  
+from ETL.get_historic_prices import DataLoader  
+from ETL.graphic import DataPlotter
+
+START_DATE = datetime(2020, 1, 1)
+END_DATE = datetime(2023, 5, 20)
+
+def process_ticker(ticker):
+    loader = DataLoader(ticker, START_DATE, END_DATE)
+    data = loader.load_data()
+    print(data)
+    plotter = DataPlotter(loader)
+    plotter.plot_data()
 
 def main():
-    start_date = datetime.datetime(2020, 1, 1)
-    end_date = datetime.datetime(2023, 5, 20)
-
-    # Descargar y graficar los datos de Bitcoin
-    bitcoin_loader = DataLoader('BTC-USD', start_date, end_date)
-    bitcoin_data = bitcoin_loader.load_data()
-    print(bitcoin_data)
-    bitcoin_plotter = DataPlotter(bitcoin_loader)
-    bitcoin_plotter.plot_data()
-
-    # Descargar y graficar los datos de Apple
-    apple_loader = DataLoader('AAPL', start_date, end_date)
-    apple_data = apple_loader.load_data()
-    print(apple_data)
-    apple_plotter = DataPlotter(apple_loader)
-    apple_plotter.plot_data()
+    for ticker in ['BTC-USD', 'AAPL']:
+        process_ticker(ticker)
 
 if __name__ == "__main__":
     main()
